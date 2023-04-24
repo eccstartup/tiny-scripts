8690d578a64d2548670dca09e6ebba5503af870c
Yi Lu <zhiwudazhanjiangshi@gmail.com>
2023-04-24 08:00:36 +0800

    title to doi url
    by Claude

---
+++ doi_find.py
@@ -0,0 +1,24 @@
+import requests
+
+
+def get_doi(title):
+    url = "https://api.crossref.org/works"
+    params = {
+        "query": title,
+        "rows": 1
+    }
+    r = requests.get(url, params=params)
+    print(r.json())
+    data = r.json()["message"]
+    if data["total-results"] > 0:
+        doi = data["items"][0]["URL"]
+        print(f"Found DOI for {title}: {doi}")
+        return doi
+    else:
+        print(f"No DOI found for {title}")
+        return None
+
+
+if __name__ == "__main__":
+    title = '联邦学习系统攻击与防御技术研究综述'
+    doi = get_doi(title)
